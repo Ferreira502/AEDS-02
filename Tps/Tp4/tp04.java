d88b71f65c761d75614f70753f6dcc8bfc863e91
Gabriel Ferreira <gabrielferreirape502@gmail.com>
2025-10-24 22:45:37 -0300

    adding tp05 mergeSort and heapSort

--- Tps/Tp4/tp04.java
+++ Tps/Tp4/tp04.java
@@ -1,9 +1,28 @@
-import java.util.*;
+/* 
+Separando cada elemento do arquivo csv
+AppID: 897820
+Name:Reigns: Game of Thrones
+Release date: "Oct 18, 2018"
+Estimated owners: 75000
+Price: 3.99
+Supported languages: "['English', 'French', 'German', 'Spanish - Spain', 'Italian', 'Japanese', 'Korean', 'Portuguese - Brazil', 'Russian', 'Simplified Chinese', 'Traditional Chinese']"
+Metacritic score: 84
+User score: 0
+Achievements: 10
+Publishers: Devolver Digital
+Developers: Nerial
+Categories: "Single-player,Steam Achievements,Full controller support"
+Genres: "Adventure,Indie,RPG"
+Tags: "RPG,Indie,Card Game,Adventure,Choices Matter,2D,Medieval,Singleplayer,Minimalist,Choose Your Own Adventure,Strategy,Story Rich,Dark Humor,Great Soundtrack,Difficult,Dark Comedy,Funny,Comedy,Cute"
+*/
+
+
 import java.io.*;
+import java.util.*;
 
 class Game 
 {
-    private int AppId;
+    private int appId;
     private String name;
     private String releaseDate;
     private int estimatedOwners;
@@ -18,13 +37,13 @@ class Game
     private String[] genres;
     private String[] tags;
 
-    public int getAppId() 
+    public int getappId() 
     { 
-        return AppId; 
+        return appId; 
     }
-    public void setAppId(int AppId) 
+    public void setappId(int appId) 
     { 
-        this.AppId = AppId; 
+        this.appId = appId; 
     }
 
     public String getName() 
@@ -144,463 +163,235 @@ class Game
         this.tags = tags; 
     }
 
-    public Game ler(String linha) 
+    public static Game ler(String linha) 
     {
         Game g = new Game();
-        String[] campos = separarCaracter(linha);
+        String[] campos = splitParaLinguaguem(linha);
 
-        for (int i = 0; i < campos.length; i++) 
-        {
-            campos[i] = removerAspas(campos[i]);
-        }
-
-        String[] dados = new String[14];
-        for (int i = 0; i < dados.length; i++) 
-        {
-            if (i < campos.length) 
-            {
-                dados[i] = campos[i];
-            } 
-            else 
-            {
-                dados[i] = "";
-            }
-        }
-
-        if (!dados[0].isEmpty()) 
-        {
-            g.setAppId(Integer.parseInt(dados[0]));
-        } 
-        else 
-        {
-            g.setAppId(0);
-        }
-
-        g.setName(dados[1]);
-        g.setReleaseDate(dados[2]);
-
-        if (!dados[3].isEmpty()) 
-        {
-            g.setEstimatedOwners(Integer.parseInt(dados[3]));
-        } 
-        else 
-        {
-            g.setEstimatedOwners(0);
-        }
-
-        if (!dados[4].isEmpty()) 
-        {
-            g.setPrice(Double.parseDouble(dados[4]));
-        } 
-        else 
-        {
-            g.setPrice(0.0);
-        }
-
-        if (!dados[5].isEmpty()) 
-        {
-            g.setSupportedLanguages(tratarArray(dados[5]));
-        } 
-        else 
-        {
-            g.setSupportedLanguages(new String[0]);
-        }
-
-        if (!dados[6].isEmpty()) 
-        {
-            g.setMetacriticScore(Integer.parseInt(dados[6]));
-        } 
-        else 
-        {
-            g.setMetacriticScore(0);
-        }
-
-        if (!dados[7].isEmpty()) 
-        {
-            g.setUserScore(Double.parseDouble(dados[7]));
-        } 
-        else 
-        {
-            g.setUserScore(0.0);
-        }
-
-        if (!dados[8].isEmpty()) 
-        {
-            g.setAchievements(Integer.parseInt(dados[8]));
-        } 
-        else 
-        {
-            g.setAchievements(0);
-        }
-
-        if (!dados[9].isEmpty()) 
-        {
-            g.setPublishers(tratarArray(dados[9]));
-        } 
-        else 
-        {
-            g.setPublishers(new String[0]);
-        }
-
-        if (!dados[10].isEmpty()) 
-        {
-            g.setDevelopers(tratarArray(dados[10]));
-        } 
-        else 
-        {
-            g.setDevelopers(new String[0]);
-        }
-
-        if (!dados[11].isEmpty()) 
-        {
-            g.setCategories(tratarArray(dados[11]));
-        } 
-        else 
-        {
-            g.setCategories(new String[0]);
-        }
-
-        if (!dados[12].isEmpty()) 
-        {
-            g.setGenres(tratarArray(dados[12]));
-        } 
-        else 
-        {
-            g.setGenres(new String[0]);
-        }
-
-        if (!dados[13].isEmpty()) 
-        {
-            g.setTags(tratarArray(dados[13]));
-        } 
-        else 
-        {
-            g.setTags(new String[0]);
-        }
+        g.appId = Integer.parseInt(colocarEspaco(campos, 0));
+        g.name = colocarEspaco(campos, 1);
+        g.releaseDate = formatarData(colocarEspaco(campos, 2));
+        g.estimatedOwners = Integer.parseInt(colocarEspaco(campos, 3));
+        g.price = Double.parseDouble(colocarEspaco(campos, 4));
+        g.supportedLanguages = stringParaArray(colocarEspaco(campos, 5));
+        g.metacriticScore = Integer.parseInt(colocarEspaco(campos, 6));
+        g.userScore = Double.parseDouble(colocarEspaco(campos, 7));
+        g.achievements = Integer.parseInt(colocarEspaco(campos, 8));
+        g.publishers = stringParaArray(colocarEspaco(campos, 9));
+        g.developers = stringParaArray(colocarEspaco(campos, 10));
+        g.categories = stringParaArray(colocarEspaco(campos, 11));
+        g.genres = stringParaArray(colocarEspaco(campos, 12));
+        g.tags = stringParaArray(colocarEspaco(campos, 13));
 
         return g;
     }
 
-    private String[] separarCaracter(String linha) 
+    private static String[] splitParaLinguaguem(String linha) 
     {
-        ArrayList<String> campos = new ArrayList<>();
-        boolean dentroAspas = false;
+        String[] campos = new String[100];
+        int count = 0;
+
         StringBuilder campoAtual = new StringBuilder();
-        
+        boolean dentroAspas = false;
+        int colchetes = 0;
+
         for (int i = 0; i < linha.length(); i++) 
         {
             char c = linha.charAt(i);
-            
-            if (c == '"') 
-            {
-                dentroAspas = !dentroAspas;
-            } 
-            else if (c == ',' && !dentroAspas) 
-            {
-                campos.add(campoAtual.toString());
-                campoAtual = new StringBuilder();
-            } 
-            else 
-            {
+
+            if (c == '\"') {
+                dentroAspas = !dentroAspas; 
+                campoAtual.append(c);
+            } else if (c == '[') {
+                colchetes++;
+                campoAtual.append(c);
+            } else if (c == ']') {
+                colchetes--;
+                campoAtual.append(c);
+            } else if (c == ',' && !dentroAspas && colchetes == 0) {
+                campos[count++] = campoAtual.toString().trim();
+                campoAtual.setLength(0);
+            } else {
                 campoAtual.append(c);
             }
         }
-        campos.add(campoAtual.toString());
-        
-        return campos.toArray(new String[0]);
+
+        // Adiciona o último campo
+        campos[count++] = campoAtual.toString().trim();
+
+        // Cria um array do tamanho exato
+        String[] resultado = new String[count];
+        for (int i = 0; i < count; i++) {
+            resultado[i] = campos[i];
+        }
+
+        return resultado;
     }
 
-    private String removerAspas(String str) 
-    {
-        if (str.length() >= 2 && str.charAt(0) == '"' && str.charAt(str.length() - 1) == '"') 
-        {
-            return str.substring(1, str.length() - 1).trim();
-        }
-        return str.trim();
-    }
 
-    private String[] tratarArray(String campo) 
-    {
-        if (campo == null || campo.isEmpty()) return new String[0];
-
-        String temp = campo;
-        if (temp.length() >= 2 && temp.charAt(0) == '[' && temp.charAt(temp.length() - 1) == ']') 
-        {
-            temp = temp.substring(1, temp.length() - 1);
-        }
-        
-        ArrayList<String> itens = new ArrayList<>();
-        StringBuilder itemAtual = new StringBuilder();
-        boolean dentroAspas = false;
-        
-        for (int i = 0; i < temp.length(); i++) 
-        {
-            char c = temp.charAt(i);
-            
-            if (c == '"') 
-            {
-                dentroAspas = !dentroAspas;
-            } 
-            else if (c == ',' && !dentroAspas) 
-            {
-                String item = itemAtual.toString().trim();
-                item = removerAspas(item);
-                if (!item.isEmpty()) 
-                {
-                    itens.add(item);
-                }
-                itemAtual = new StringBuilder();
-            } 
-            else 
-            {
-                itemAtual.append(c);
-            }
-        }
-        
-        String ultimoItem = itemAtual.toString().trim();
-        ultimoItem = removerAspas(ultimoItem);
-        if (!ultimoItem.isEmpty()) 
-        {
-            itens.add(ultimoItem);
-        }
-
-        return itens.toArray(new String[0]);
-    }
-
-    private String formatarData(String data) 
+    private static String formatarData(String data) 
     {
         if (data == null || data.isEmpty()) 
         {
             return "";
         }
 
-        String[] partes = separarEspacos(data);
-        if (partes.length != 3) 
+        String[] partes = data.split(" ");
+        
+        if (partes.length < 3) 
         {
             return data;
         }
 
-        String mes = partes[0];
-        String dia = partes[1];
-        if (dia.length() > 0 && dia.charAt(dia.length() - 1) == ',') 
-        {
-            dia = dia.substring(0, dia.length() - 1);
-        }
-        String ano = partes[2];
+        String mesTexto = partes[0];
+        String diaTexto = partes[1].replace(",", "");
+        String anoTexto = partes[2];
 
-        int diaInt = Integer.parseInt(dia);
+        int dia = Integer.parseInt(diaTexto);
+
         String diaFormatado;
-        if (diaInt < 10)
+        if (dia < 10) 
         {
-            diaFormatado = "0" + diaInt;
+            diaFormatado = "0" + dia;
         } 
         else 
         {
-            diaFormatado = String.valueOf(diaInt);
+            diaFormatado = String.valueOf(dia);
         }
 
-        String mesNum = "";
-        switch (mes) 
+        String mesFormatado;
+        switch (mesTexto) 
         {
-            case "Jan": mesNum = "01"; break;
-            case "Feb": mesNum = "02"; break;
-            case "Mar": mesNum = "03"; break;
-            case "Apr": mesNum = "04"; break;
-            case "May": mesNum = "05"; break;
-            case "Jun": mesNum = "06"; break;
-            case "Jul": mesNum = "07"; break;
-            case "Aug": mesNum = "08"; break;
-            case "Sep": mesNum = "09"; break;
-            case "Oct": mesNum = "10"; break;
-            case "Nov": mesNum = "11"; break;
-            case "Dec": mesNum = "12"; break;
-            default: mesNum = "00"; break;
+            case "Jan": mesFormatado = "01"; break;
+            case "Feb": mesFormatado = "02"; break;
+            case "Mar": mesFormatado = "03"; break;
+            case "Apr": mesFormatado = "04"; break;
+            case "May": mesFormatado = "05"; break;
+            case "Jun": mesFormatado = "06"; break;
+            case "Jul": mesFormatado = "07"; break;
+            case "Aug": mesFormatado = "08"; break;
+            case "Sep": mesFormatado = "09"; break;
+            case "Oct": mesFormatado = "10"; break;
+            case "Nov": mesFormatado = "11"; break;
+            case "Dec": mesFormatado = "12"; break;
+            default: mesFormatado = "00"; break;
         }
 
-        return diaFormatado + "/" + mesNum + "/" + ano;
+        return diaFormatado + "/" + mesFormatado + "/" + anoTexto;
     }
 
-    private String[] separarEspacos(String str) 
+    private static String colocarEspaco(String[] c, int i) 
     {
-        ArrayList<String> partes = new ArrayList<>();
-        StringBuilder parteAtual = new StringBuilder();
-        boolean dentroPalavra = false;
-        
-        for (int i = 0; i < str.length(); i++) 
-        {
-            char c = str.charAt(i);
-            
-            if (c == ' ' || c == '\t') 
-            {
-                if (dentroPalavra) 
-                {
-                    partes.add(parteAtual.toString());
-                    parteAtual = new StringBuilder();
-                    dentroPalavra = false;
-                }
-            } 
-            else 
-            {
-                parteAtual.append(c);
-                dentroPalavra = true;
-            }
-        }
-        
-        if (dentroPalavra) 
-        {
-            partes.add(parteAtual.toString());
-        }
-        
-        return partes.toArray(new String[0]);
+        if (i >= c.length) return "";
+        return c[i].replace("\"", "").trim();
     }
 
-    private String formatarArray(String[] arr) 
+    private static String[] stringParaArray(String campo) 
     {
-        if (arr == null || arr.length == 0) 
-        {
-            return "[]";
-        }
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("[");
-        
-        for (int i = 0; i < arr.length; i++) 
-        {
-            if (i > 0) 
-            {
-                sb.append(", ");
-            }
-            String item = arr[i];
-            if (item.length() >= 2) 
-            {
-                char primeiro = item.charAt(0);
-                char ultimo = item.charAt(item.length() - 1);
-                if ((primeiro == '\'' && ultimo == '\'') || (primeiro == '"' && ultimo == '"')) 
-                {
-                    item = item.substring(1, item.length() - 1);
-                }
-            }
-            sb.append(item);
-        }
-        
-        sb.append("]");
-        return sb.toString();
+        campo = campo.replace("[", "").replace("]", "").replace("'", "").replace("\"", "").trim();
+        if (campo.isEmpty()) return new String[0];
+        return campo.split(",");
     }
 
     public String formatarSaida() 
     {
-        return "=> " + AppId +
-                " ## " + name +
-                " ## " + formatarData(releaseDate) +
-                " ## " + estimatedOwners +
-                " ## " + price +
-                " ## " + formatarArray(supportedLanguages) +
-                " ## " + metacriticScore +
-                " ## " + userScore +
-                " ## " + achievements +
-                " ## " + formatarArray(publishers) +
-                " ## " + formatarArray(developers) +
-                " ## " + formatarArray(categories) +
-                " ## " + formatarArray(genres) +
-                " ## " + formatarArray(tags) +
-                " ##";
+        return "=> " + appId + " ## " + name + " ## " + releaseDate + " ## " + estimatedOwners
+                + " ## " + price + " ## " + Arrays.toString(supportedLanguages) + " ## " + metacriticScore
+                + " ## " + userScore + " ## " + achievements + " ## " + Arrays.toString(publishers)
+                + " ## " + Arrays.toString(developers) + " ## " + Arrays.toString(categories)
+                + " ## " + Arrays.toString(genres) + " ## " + Arrays.toString(tags) + " ##";
     }
 }
 
-public class tp04 
+public class tp04
 {
     public static void main(String[] args) 
     {
         Game[] lista = new Game[100000];
         int count = 0;
 
-        File arquivo = new File("/tmp/games.csv");
-        Scanner leitorArquivo = null;
-        
-        leitorArquivo = criarScanner(arquivo);
-        if (leitorArquivo != null) 
+        File file = new File("games.csv");
+        Scanner arquivo = null;
+
+        try 
         {
-            if (leitorArquivo.hasNextLine()) leitorArquivo.nextLine(); 
-
-            while (leitorArquivo.hasNextLine() && count < lista.length) 
-            {
-                String linha = leitorArquivo.nextLine();
-                if (linha.trim().isEmpty()) continue;
-
-                Game g = new Game();
-                g = g.ler(linha);
-                lista[count] = g;
-                count++;
-            }
-            leitorArquivo.close();
+            arquivo = new Scanner(file);
+        } 
+        catch (FileNotFoundException e) 
+        {
+            System.out.println(""); //arquivo nao encontrado
+            return;
         }
 
-        Scanner sc = new Scanner(System.in);
+        if (arquivo.hasNextLine()) //pular a primeira linha
+        { 
+            arquivo.nextLine();
+        }   
+        
+        while (arquivo.hasNextLine() && count < lista.length) 
+        {
+            String linha = arquivo.nextLine();
+            if (linha.trim().isEmpty()) continue;
+            lista[count++] = Game.ler(linha);
+        }
+        arquivo.close();
+
+        Scanner sc = new Scanner(System.in); //entrada do id
+
         while (true) 
         {
-            String entrada = sc.nextLine();
+            String entrada = sc.nextLine().trim();
 
-            if (entrada.length() == 3    &&
-                entrada.charAt(0) == 'F' &&
-                entrada.charAt(1) == 'I' &&
-                entrada.charAt(2) == 'M') 
-                {
+            if (entrada.equals("FIM")) 
+            {
                 break;
             }
 
-            if (isInteger(entrada)) 
+            // Verifica se a entrada contém apenas números
+            boolean entradaValida = true;
+            for (int i = 0; i < entrada.length(); i++) 
+            {
+                char c = entrada.charAt(i);
+                if (c < '0' || c > '9') 
+                {
+                    entradaValida = false;
+                    break;
+                }
+            }
+
+            if (entradaValida) 
             {
                 int id = Integer.parseInt(entrada);
-                boolean encontrado = false;
+                Game jogoEncontrado = null;
 
+                // Busca o jogo pelo ID
                 for (int i = 0; i < count; i++) 
                 {
-                    if (lista[i].getAppId() == id) 
+                    if (lista[i].getappId() == id) 
                     {
-                        System.out.println(lista[i].formatarSaida());
-                        encontrado = true;
+                        jogoEncontrado = lista[i];
                         break;
                     }
                 }
 
-                if (!encontrado) 
+                if (jogoEncontrado != null)
                 {
-                    System.out.println("");
+                    System.out.println(jogoEncontrado.formatarSaida());
+                } 
+                else 
+                {
+                    System.out.println(""); // não achou
                 }
+
             } 
             else 
             {
-                System.out.println("");
+                System.out.println(""); // entrada inválida
             }
+
+
         }
+
         sc.close();
-    }
-    
-    private static Scanner criarScanner(File arquivo) 
-    {
-        try 
-        {
-            return new Scanner(arquivo);
-        } 
-        catch (FileNotFoundException e) 
-        {
-            return null;
-        }
-    }
-    
-    private static boolean isInteger(String str) 
-    {
-        if (str == null || str.isEmpty()) 
-        {
-            return false;
-        }
-        for (int i = 0; i < str.length(); i++) 
-        {
-            if (!Character.isDigit(str.charAt(i))) 
-            {
-                return false;
-            }
-        }
-        return true;
     }
 }
